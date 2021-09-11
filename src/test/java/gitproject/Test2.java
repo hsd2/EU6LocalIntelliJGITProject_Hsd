e3c72a072b6dcb19c834a9316fb5fa1d53abbe62
hsd2 <hsd2@hotmail.com>
2021-09-11 20:15:18 +0100

    My third commit, test2 file added

--- src/test/java/gitproject/Test2.java
+++ src/test/java/gitproject/Test2.java
@@ -3,6 +3,6 @@ package gitproject;
 public class Test2 {
     public static void main(String[] args) {
 
-        System.out.println("My second file");
+        System.out.println("My second file ");
     }
 }
